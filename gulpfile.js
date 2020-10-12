12ffea95488d74da3eeb3997df60655e26ca1e1f
unsxxndxrk <selincorvin@yahoo.com>
2020-10-12 16:01:00 +0300

    refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,70 @@
-var 	
-	gulp         = require('gulp'),
-	sass         = require('gulp-sass'),
-	browserSync  = require('browser-sync'),
-	autoprefixer = require('gulp-autoprefixer'),
-	notify       = require('gulp-notify'),
-	imagemin     = require('gulp-imagemin'), 
-    pngquant     = require('imagemin-pngquant'), 
-    uglify       = require('gulp-uglify-es').default,
-    cleanCSS     = require('gulp-clean-css'),
-    cache        = require('gulp-cache');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const autoprefixer = require("gulp-autoprefixer");
+const notify = require("gulp-notify");
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
+const uglify = require("gulp-uglify-es").default;
+const cleanCSS = require("gulp-clean-css");
+const cache = require("gulp-cache");
 
-// localhost and autoreaload
-gulp.task('browser-sync', function(done) {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false
-	});
-	done();
+gulp.task("browser-sync", (done) => {
+  browserSync({
+    server: {
+      baseDir: "src",
+    },
+    notify: false,
+  });
+  done();
 });
 
-gulp.task('sass', function() {
-	return gulp.src('app/scss/**/*.scss')
-		.pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
-		.pipe(autoprefixer(['last 15 versions']))
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({stream: true}));
+gulp.task("sass", () => {
+  return gulp
+    .src("src/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", notify.onError()))
+    .pipe(autoprefixer(["last 15 versions"]))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('watch', function(cb) {
-	gulp.parallel(
-		'sass', 
-		'browser-sync'
-	)(cb);
-	gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-	gulp.watch('app/*.html').on('change', browserSync.reload);
+gulp.task("watch", (cb) => {
+  gulp.parallel("sass", "browser-sync")(cb);
+  gulp.watch("src/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-gulp.task('img', function() {
-	return gulp.src('app/img/**/*')
-  		.pipe(cache(imagemin({ 
-	    	interlaced: true,
-		    progressive: true,
-		    svgoPlugins: [{removeViewBox: false}],
-		    use: [pngquant()]
-		})))
-		.pipe(gulp.dest('dist/img'));
+gulp.task("img", () => {
+  return gulp
+    .src("src/img/**/*")
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true,
+          progressive: true,
+          svgoPlugins: [{ removeViewBox: false }],
+          use: [pngquant()],
+        })
+      )
+    )
+    .pipe(gulp.dest("dist/img"));
 });
 
-gulp.task('clear', function() {
-    return cache.clearAll();
+gulp.task("clear", () => {
+  return cache.clearAll();
 });
 
-gulp.task('build', gulp.series('clear', 'img', function(cb) {
-    var buildCss = gulp.src([
-        'app/css/**/*'
-        ])
-    .pipe(gulp.dest('dist/css'))
-
-    var buildFonts = gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
-
-    var buildLibs = gulp.src('app/libs/**/*')
-    .pipe(gulp.dest('dist/libs'))
-
-    var buildJs = gulp.src([
-        'app/js/**/*'
-        ]) 
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-
-    var buildHtml = gulp.src('app/*.html')
-    .pipe(gulp.dest('dist'));
+gulp.task(
+  "build",
+  gulp.series("clear", "img", (cb) => {
+    gulp.src(["src/css/**/*"]).pipe(gulp.dest("dist/css"));
+    gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+    gulp.src("src/libs/**/*").pipe(gulp.dest("dist/libs"));
+    gulp.src(["src/js/**/*"]).pipe(uglify()).pipe(gulp.dest("dist/js"));
+    gulp.src("src/*.html").pipe(gulp.dest("dist"));
     cb();
-}));
+  })
+);
 
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("watch"));
